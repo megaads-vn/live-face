b7523a872628cb5a3179bf5954fc2ed99262d7a5
Bach Nguyen <bachnx2303@gmail.com>
2019-08-08 14:14:12 +0700

    fix: change target point

--- controller/live_face_recognition.py
+++ controller/live_face_recognition.py
@@ -119,7 +119,7 @@ class LiveFaceRecognition(Resource):
                     name = "unknown"
                     confidence = "  {0}%".format(round(100 - confidence))
 
-                if target >= 30:
+                if target >= 40:
                     cv2.putText(img, str(name), (x+5,y-5), font, 1, (255,255,255), 2)
                     cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
 
